5dc8e24acc233dcdc83df2ac10bdcebcb3f4723f
ElliotScher <ecscher84@gmail.com>
2025-06-02 12:48:58 -0400

    remove build.bat

--- build.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-pyinstaller main.py ^
-    --add-data "util\calibration\test_calibration.json;util\calibration" ^
-    --onefile ^
-    --windowed ^
-    --hidden-import=tkinter ^
-    --hidden-import=matplotlib.backends.backend_tkagg ^
-    --hidden-import=PIL._tkinter_finder
