2092b44d8caa29fceb63aa10070b21081721cbe8
ElliotScher <ecscher84@gmail.com>
2025-06-10 18:44:34 -0400

    remove binary

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 pyinstaller --clean \
-  --add-binary "/usr/lib/aarch64-linux-gnu/libpython3.12.so.1.0:." \
   --add-data "util/calibration/test_calibration.json:util/calibration" \
   --onefile \
   --windowed \
