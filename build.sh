f154aae199723968df709383bb16052008c4b9b7
ElliotScher <ecscher84@gmail.com>
2025-06-10 18:14:12 -0400

    update build script

--- build.sh
+++ build.sh
@@ -4,5 +4,6 @@ pyinstaller main.py \
             --windowed \
             --hidden-import=tkinter \
             --hidden-import=matplotlib.backends.backend_tkagg \
+            --hidden-import=libpython3.12.so \
             --hidden-import=PIL._tkinter_finder \
             --noconfirm
