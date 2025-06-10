d3d902665b5735b4dd2a13b691879ad8299c58bd
ElliotScher <ecscher84@gmail.com>
2025-06-10 18:38:19 -0400

    add clean.sh

---
+++ clean.sh
@@ -0,0 +1,10 @@
+pyinstaller --clean \
+  --add-binary "/usr/lib/aarch64-linux-gnu/libpython3.12.so.1.0:." \
+  --add-data "util/calibration/test_calibration.json:util/calibration" \
+  --onefile \
+  --windowed \
+  --hidden-import=tkinter \
+  --hidden-import=matplotlib.backends.backend_tkagg \
+  --hidden-import=PIL._tkinter_finder \
+  --noconfirm \
+  main.py
