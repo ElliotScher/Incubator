66fe49511ae45d698e416a769f46af6132bb6223
ElliotScher <ecscher84@gmail.com>
2025-06-11 15:37:18 -0400

    fix

--- util/uart_util.py
+++ util/uart_util.py
@@ -10,18 +10,18 @@ class UARTUtil:
     def send_data(ser, data):
         if isinstance(data, str):
             data += '\n'
-            data = data.encode('utf-8')
+            data = data.encode('ascii')
         ser.write(data)
 
     @staticmethod
     def receive_data(ser, size=64):
-        return ser.read(size).decode('utf-8', errors='ignore')
+        return ser.read(size).decode('ascii', errors='ignore')
 
     @staticmethod
     def send_and_receive(ser, baudrate=9600, data='', timeout=1, response_size=64, delay=0.1):
             if isinstance(data, str):
                 data += '\n'  # <-- Adjust for your device
-                data = data.encode('utf-8')  # <-- Use 'ascii' if utf-8 causes issues
+                data = data.encode('ascii')  # <-- Use 'ascii' if ascii causes issues
             ser.reset_input_buffer()
             ser.write(data)
-            return ser.read(response_size).decode('utf-8', errors='ignore')  # or 'utf-8'
+            return ser.read(response_size).decode('ascii', errors='ignore')  # or 'utf-8'
