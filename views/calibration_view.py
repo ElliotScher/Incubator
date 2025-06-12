e056c5ba8e426c6cabf3c141b68c76ee6532acc3
ElliotScher <ecscher84@gmail.com>
2025-06-12 13:31:26 -0400

    python

--- views/calibration_view.py
+++ views/calibration_view.py
@@ -139,7 +139,7 @@ class CalibrationView(tk.Frame):
                 line = line.strip()
                 # Check for "OD:" prefix and extract the number
                 if "OD:" in line:
-                    print("Received line:", line)
+                    print("Received line:", line, "\n\n\n\n\n")
                     try:
                         number_str = line[3:]  # Everything after "OD:"
                         number = float(number_str)
