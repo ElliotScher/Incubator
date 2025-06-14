c783a0771582039b719c657f01590f92eb6e50b0
ElliotScher <ecscher84@gmail.com>
2025-06-14 18:56:51 -0400

    apt

--- views/menu_view.py
+++ views/menu_view.py
@@ -12,13 +12,16 @@ class MenuView(tk.Frame):
         label.grid(row=0, column=0, sticky="nsew")
 
         button = tk.Button(self, text="Calibration",
+                           font=("Arial", 56),
                            command=lambda: controller.show_frame("CalibrationView"))
         button.grid(row=0, column=1, sticky="nsew")
 
         button2 = tk.Button(self, text="Connection",
+                            font=("Arial", 56),
                             command=lambda: controller.show_frame("ConnectionView"))
         button2.grid(row=1, column=0, sticky="nsew")
 
         button3 = tk.Button(self, text="Run",
+                            font=("Arial", 56),
                             command=lambda: controller.show_frame("RunView"))
         button3.grid(row=1, column=1, sticky="nsew")
