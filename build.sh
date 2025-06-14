c447398395cdfe92e4ad6f7343f6cfc7ca3cda0b
ElliotScher <ElliotScher@users.noreply.github.com>
2025-06-14 21:59:00 +0000

    deploy: f3a2549c783f7420b0958e9b8b19313318c15c9b

--- build.sh
+++
@@ -1,9 +0,0 @@
-pyinstaller --clean \
-  --add-data "util/calibration/test_calibration.json:util/calibration" \
-  --onefile \
-  --windowed \
-  --hidden-import=tkinter \
-  --hidden-import=matplotlib.backends.backend_tkagg \
-  --hidden-import=PIL._tkinter_finder \
-  --noconfirm \
-  main.py
